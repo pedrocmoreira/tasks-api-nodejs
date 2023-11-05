a0e02d727e890cb980bc8162963794c7be70c250
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-11-05 11:28:04 -0300

    configurando server.js e comportamento de routes

---
+++ src/utils/extract-query-params.js
@@ -0,0 +1,9 @@
+export function extractQueryParams(query){
+  return query.substr(1).split('&').reduce((queryParams, param) => {
+    const [key, value] = params.split('=');
+
+    queryParams[key] = value;
+
+    return queryParams;
+  }, {});
+}
