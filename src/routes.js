8580442af0fdd5731232dc5f56210116b1675a41
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-11-05 11:44:19 -0300

    adicionando rota de post

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from "./database";
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const {title, description} = req.body;
+
+      if (!title) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'title is required' }),
+        )
+      }
+
+      if (!description) {
+        return res.writeHead(400).end(
+          JSON.stringify({message: 'description is required' })
+        )
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  },
+  
+]
